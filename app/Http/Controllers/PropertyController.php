9e1833ddc9cf845b77ddc98013fc222aaff0a75b
Rich Guo <richguo0615@gmail.com>
2022-01-08 23:41:49 +0800

    update: sql

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 class PropertyController extends Controller
 {
     public function getTopsByOrderNum(Request $request) {
-        $data = DB::select('SELECT p.id, p.`name`, COUNT(o.room_id) AS order_nums FROM properties AS p
+        $data = DB::select('SELECT p.id, p.`name`, COUNT(*) AS order_nums FROM properties AS p
                             LEFT JOIN rooms AS r ON p.id = r.property_id
                             INNER JOIN orders AS o ON r.id = o.room_id
                             GROUP BY r.id
