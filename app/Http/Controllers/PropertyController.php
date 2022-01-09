b69a3fa9f974be532dbfebc7bfc276b150eadd4c
Rich Guo <richguo0615@gmail.com>
2022-01-09 16:23:13 +0800

    update: sql - getTopsByOrderNum - 篩選 2021 年 2 月份

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -8,12 +8,16 @@ use Illuminate\Support\Facades\DB;
 class PropertyController extends Controller
 {
     public function getTopsByOrderNum(Request $request) {
-        $data = DB::select('SELECT p.id, p.`name`, COUNT(*) AS order_nums FROM properties AS p
+        $start = '2021-02-01 00:00:00';
+        $end = '2021-02-28 23:59:59';
+
+        $data = DB::select("SELECT p.id, p.`name`, COUNT(*) AS order_nums FROM properties AS p
                             LEFT JOIN rooms AS r ON p.id = r.property_id
                             INNER JOIN orders AS o ON r.id = o.room_id
+                            WHERE o.created_at BETWEEN DATE(?) AND DATE(?)
                             GROUP BY r.id
                             ORDER BY order_nums DESC, p.id ASC
-                            LIMIT 10;');
+                            LIMIT 10;", [$start, $end]);
 
         return response()->json($data);
     }
